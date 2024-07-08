e37181512d37ca9008926253d0cc004c0da1abfc
JoyB00 <dendypramartha03@gmail.com>
2024-07-08 12:03:43 +0700

    update CV

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -43,20 +43,6 @@ export default function Navbar() {
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
           <FontAwesomeIcon icon={faBars} />
-          {/* <svg
-            className="h-6 w-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16m-7 6h7"
-            ></path>
-          </svg> */}
         </button>
       </div>
       {isMenuOpen && (
